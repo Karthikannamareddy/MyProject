acdb8547fa8922863126963d43abfda23218d65c
Karthikannamareddy <126660956+Karthikannamareddy@users.noreply.github.com>
2023-03-28 06:36:37 +0000

    backend server

--- src/db/init.ts
+++
@@ -1,9 +0,0 @@
-import { createAdminUser } from "./script";
-import User from "./models/users";
-
-const isDev = true
-const dbInit = async() => {
-    await User.sync({alter:isDev});
-
-}
-export default dbInit
